ce6e8defb7ab64d23495a861241db8477a970396
Zeliha Arslan <72877610+Zelihaars@users.noreply.github.com>
2023-06-17 21:46:12 +0300

    Case-No :1

--- lib/core/providers/firebase_providers.dart
+++ lib/core/providers/firebase_providers.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
 final firestoreProvider= Provider((ref)=>FirebaseFirestore.instance);
 final storageProvider= Provider((ref)=>FirebaseStorage.instance);
 final authProvider= Provider((ref)=>FirebaseAuth.instance);
